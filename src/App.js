707316831517f0e90ed7a0d8a4ba5348cfbd062f
Sunjun Hwang <sunjun.software@gmail.com>
2025-03-27 13:15:02 +0900

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row
+        style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontFamily: "Montserrat",
+            fontSize: "3rem",
+        }}
+      >
+        SWOT Analysis
+        <div className="swot-chart">
+          <div className="swot-div">
+            <h2>Strengths</h2>
+          </div>
+          <div className="swot-div">
+            <h2>Weaknesses</h2>
+          </div>
+          <div className="swot-div">
+            <h2>Opportunities</h2>
+          </div>
+          <div className="swot-div">
+            <h2>Threats</h2>
+          </div>
+        </div>
+      </Row>
+    </Container>
   );
 }
 
